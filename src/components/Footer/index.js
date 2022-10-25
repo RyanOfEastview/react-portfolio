597265ac841c7da0063ed0de6d982fdc764a2f15
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-25 13:41:24 -0400

    Add footer functionality

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { icon } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons'
+
 
 function Footer() {
-
+    console.log()
     return (
-        <footer className="my-5" >
-            <h1>Footer</h1>
-            {/* <i class="fa-brands fa-github"></i> */}
-            <FontAwesomeIcon icon="fa-brands fa-github" />
+        <footer className="min-vh-100 py-2">
+
+            <a href="https://github.com/HiuSumJaimeYue"  target="_blank" rel="noopener noreferrer"> 
+            <FontAwesomeIcon icon={faGithub} size="4x" className="footer-icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/hiu-sum-jaime-yue-2b8a1b1b9/"  target="_blank" rel="noopener noreferrer"> 
+            <FontAwesomeIcon icon={faLinkedin} size="4x" className="footer-icon" />
+            </a>
+            <a href="https://m.facebook.com/jaime.yue/"  target="_blank" rel="noopener noreferrer"> 
+            <FontAwesomeIcon icon={faFacebook} size="4x" className="footer-icon" />
+            </a>
+
         </footer>
     )
 }
