f7975db0bc63f572c349f9cecaaef5a0a1550d6f
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-25 17:59:35 -0400

    Fix Contact css again

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -39,11 +39,11 @@ function ContactForm() {
         <section>
             <h1 data-testid="h1tag">Contact me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
-                <div>
+                <div className="contact-name">
                     <label htmlFor="name">Name:</label>
                     <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
                 </div>
-                <div>
+                <div className="contact-email">
                     <label htmlFor="email">Email address:</label>
                     <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
                 </div>
