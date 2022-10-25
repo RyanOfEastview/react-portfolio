b5df7a02bfbcc95a93b2622713e471cbdf1bd231
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-25 11:25:59 -0400

    Add css to About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,12 +6,12 @@ function About() {
     return (
         <section className="my-5" >
             <h1 id="about">About Me</h1>
-            <img src={selfPic} className="my-2" alt="self-pic" />
+            <img src={selfPic} className="my-2" id="about-pic" alt="self-pic" />
             <div className="my-2">
                 <p>
                     My name is Hiu Sum Jaime Yue. I am a fourth-year Computer Systems
-                    Engineering student in Carleton University that will gradurate in June 2023.
-                    Other than computer architecture and software, I am also interested in the web development. 
+                    Engineering student in Carleton University that will graduate in June 2023.
+                    Other than computer architecture and software, I am also interested in Web Development. 
                     My future goal is to learn machine learning. 
                 </p>
             </div>
