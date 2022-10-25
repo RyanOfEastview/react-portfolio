53e2ef78318960f9a62d8009ec149c7c60947a7e
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-25 17:18:48 -0400

    Restore function capitalizeFirstLetter and fix mistakes on contact-form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,52 +1,55 @@
 import React, { useState } from 'react';
-import { validateEmail } from '../../utils/helpers';
+
+import { validateEmail, capitalizeFirstLetter } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    const { name, email, message } = formState;
     const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
 
-    function handleChange(e) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            console.log('Submit Form', formState);
+        }
+    };
+
+    const handleChange = (e) => {
         if (e.target.name === 'email') {
             const isValid = validateEmail(e.target.value);
-            console.log(isValid);
-            // isValid conditional statement
             if (!isValid) {
                 setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
             }
-            else {
-                if (!e.target.value.length) {
-                    setErrorMessage(`${e.target.name} is required.`);
-                } else {
-                    setErrorMessage('');
-                }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${capitalizeFirstLetter(e.target.name)} is required.`);
+            } else {
+                setErrorMessage('');
             }
         }
         if (!errorMessage) {
             setFormState({ ...formState, [e.target.name]: e.target.value });
+            console.log('Handle Form', formState);
         }
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
-    }
+    };
 
     return (
         <section>
-            <h1>Contact me</h1>
+            <h1 data-testid="h1tag">Contact me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
-                <div className="contact-name">
+                <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
                 </div>
-                <div className="contact-email">
+                <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
                 </div>
                 {errorMessage && (
                     <div>
@@ -56,7 +59,7 @@ function ContactForm() {
                 <button data-testid="button" type="submit">Submit</button>
             </form>
         </section>
-    )
+    );
 }
 
 export default ContactForm;
