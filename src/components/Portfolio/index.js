e6b7b42484f90aad804d0486f0c57659718a9120
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-26 00:28:43 -0400

    Add 6 projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Portfolio() {
     const projects = [
-        {name: 'Code Quiz', link: "www.abc.com"},
-        {},
-        {}
+        {name: 'Music Finder', link: "www.abc.com", imageName:"liveMusicFinderPreview"},
+        {name: 'Movies Review', link: "www.abc.com", imageName:"MoviesReviewPreview"},
+        {name: 'Weather Dashboard', link: "www.abc.com", imageName:"weatherDashboardPreview"},
+        {name: 'Work Day Schedule', link: "www.abc.com", imageName:"workDaySchedulerPreview"},
+        {name: 'NoteTaker', link: "www.abc.com", imageName:"noteTakerPreview"},
+        {name: 'Tech Blog', link: "www.abc.com", imageName:"TechBlogPreview"},
+        
+
     ]
     return (
         <div className="my-5" >
@@ -13,12 +18,14 @@ function Portfolio() {
                 <div>
                     <div className="flex-row">
                         {projects.map((project, i) => (
-                            <img
-                                src={require(`../../assets/images/liveMusicFinderPreview.jpg`)}//${category}/${i}
+                            <div>
+                                <img
+                                src={require(`../../assets/images/${project.imageName}.jpg`)}//${category}/${i}
                                 alt={project.name}
                                 className="img-thumbnail mx-1"
                              //key={project.name}//<img src="./assets/images/liveMusicFinderPreview.jpg" alt="Live Music Finder Preview">
                             />
+                            </div>
                         ))}
                     </div>
                 </div>
